2ef201ad7f8677685d0a424d825e7f266a2f43a0
DebjitPramanick <dpramanick9990@gmail.com>
2021-05-08 09:54:35 +0530

    Category filter

--- news/views.py
+++ news/views.py
@@ -4,8 +4,13 @@ import json
 from django.http import JsonResponse
 # Create your views here.
 
+countries = {'ae': 'UAE', 'ar': 'Argentina', 'at': 'Austria', 'au': 'Australia', 'be': 'Belgium', bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it jp k rl tl vm am xm yn gn ln on zp hp lp tr or sr us as es gs is kt ht rt wu au sv ez}
+
+
+
 def index(req):
-    allNews = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=291fabaef53b4aa48a118f8235a1ba3c')
+    
+    allNews = requests.get('https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=291fabaef53b4aa48a118f8235a1ba3c')
     res = json.loads(allNews.content)
     return render(req, 'index.html', {'res': res})
 
@@ -13,6 +18,12 @@ def query(req):
     searchRes = {}
     if req.GET:
         query = req.GET.get('query').lower()
-        queryNews = requests.get('https://newsapi.org/v2/everything?q='+query+'&apiKey=291fabaef53b4aa48a118f8235a1ba3c')
+        queryNews = requests.get('https://newsapi.org/v2/everything?q='+query+'&pageSize=100&apiKey=291fabaef53b4aa48a118f8235a1ba3c')
         searchRes = json.loads(queryNews.content)
         return render(req,'index.html', {'res': searchRes})
+
+def getCategory(req, ct):
+    searchRes = {}
+    catNews = requests.get('https://newsapi.org/v2/top-headlines?country=in&pageSize=100&category='+ct+'&apiKey=291fabaef53b4aa48a118f8235a1ba3c')
+    searchRes = json.loads(catNews.content)
+    return render(req,'index.html', {'res': searchRes})
